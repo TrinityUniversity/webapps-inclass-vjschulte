a79532a982f88a9fc70907b289f3ab73667b542b
vjschulte <vschulte@trinity.edu>
2023-10-21 23:17:58 -0500

    Sessions and User Logins

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject._
+
 import models.TaskListInMemoryModel
 
 import play.api.mvc._
@@ -22,7 +23,7 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
             val username = args("username").head
             val password = args("password").head
             if(TaskListInMemoryModel.validateUser(username, password)) {
-                Redirect(routes.TaskList1.taskList)
+                Redirect(routes.TaskList1.taskList).withSession("username" -> username)
             } else {
                 Redirect(routes.TaskList1.login)
             }
@@ -35,17 +36,24 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
             val username = args("username").head
             val password = args("password").head
             if(TaskListInMemoryModel.createUser(username, password)) {
-                Redirect(routes.TaskList1.taskList)
+                Redirect(routes.TaskList1.taskList).withSession("username" -> username)
             } else {
                 Redirect(routes.TaskList1.login)
             }
         }.getOrElse(Redirect(routes.TaskList1.login))
     }
 
-    def taskList = Action {
-        val username = "Mark"
-        val tasks = TaskListInMemoryModel.getTasks(username)
-        Ok(views.html.taskList1(tasks))
+    def taskList = Action { request =>
+        val usernameOption = request.session.get("username")
+        usernameOption.map { username =>
+            val tasks = TaskListInMemoryModel.getTasks(username)
+            Ok(views.html.taskList1(tasks))
+        }.getOrElse(Redirect(routes.TaskList1.login))
+        
+    }
+
+    def logout = Action {
+        Redirect(routes.TaskList1.login).withNewSession
     }
 
 }
