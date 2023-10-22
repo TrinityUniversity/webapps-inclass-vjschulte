42409cb61a0c1cdbfde04197c6cecbde0831370b
vjschulte <vschulte@trinity.edu>
2023-10-22 17:39:15 -0500

    More Testing

--- server/test/TaskListInMemoryModelSpec.scala
+++ server/test/TaskListInMemoryModelSpec.scala
@@ -12,9 +12,36 @@ class TaskListInMemoryModelSpec extends PlaySpec {
         "reject login with wrong username" in {
             TaskListInMemoryModel.validateUser("Mike", "pass") mustBe (false)
         }
+        "reject login with wrong username and password" in {
+            TaskListInMemoryModel.validateUser("Mike", "password") mustBe (false)
+        }
 
         "get correct default tasks" in {
             TaskListInMemoryModel.getTasks("Mark") mustBe (List("Make Videos", "eat", "sleep", "code"))
+        }
+
+        "create new user with no tasks" in {
+            TaskListInMemoryModel.createUser("Mike", "password") mustBe (true)
+            TaskListInMemoryModel.getTasks("Mike") mustBe (Nil)
+        }
+
+        "create user with existing name" in {
+            TaskListInMemoryModel.createUser("Mark", "password") mustBe (false)
+        }
+
+        "add new task for default user" in {
+            TaskListInMemoryModel.addTask("Mark", "testing")
+            TaskListInMemoryModel.getTasks("Mark") must contain ("testing")
+        }
+
+        "add new task for new user" in {
+            TaskListInMemoryModel.addTask("Mike", "testing1")
+            TaskListInMemoryModel.getTasks("Mike") must contain ("testing1")
+        }
+
+        "remove task from default user" in {
+            TaskListInMemoryModel.removeTask("Mark", TaskListInMemoryModel.getTasks("Mark").indexOf("eat"))
+            TaskListInMemoryModel.getTasks("Mark") must not contain ("eat")
         }
     }
 }
