287914ddd445456f761aafbf09c76e0019657108
vjschulte <vschulte@trinity.edu>
2023-10-21 23:25:57 -0500

    CSRF implementation

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -9,7 +9,7 @@ import play.api.mvc._
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-    def login = Action {
+    def login = Action { implicit request => 
         Ok(views.html.login1())
     }
 
