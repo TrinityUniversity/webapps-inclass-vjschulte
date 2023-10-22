dc69a29d3a7c77740d4857e277943ebdf6bfdbca
vjschulte <vschulte@trinity.edu>
2023-10-21 22:18:32 -0500

    Form Data with POST + Reverse Routing

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -15,6 +15,16 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
         Ok(s"$username logged in with $password.")
     }
 
+    def validateLoginPost = Action { request =>
+        val postVals = request.body.asFormUrlEncoded
+        postVals.map { args => 
+            val username = args("username").head
+            val password = args("password").head
+            //play functionality - looks up the route for you if things check out "Reverse Routing"
+            Redirect(routes.TaskList1.taskList)
+        }.getOrElse(Redirect(routes.TaskList1.login))
+    }
+
     def taskList = Action {
         val tasks = List("task1","task2","task3","sleep","eat")
         Ok(views.html.taskList1(tasks))
