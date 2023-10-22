735bc68f2f7afb1813e6e31de5151e5575f4ad39
vjschulte <vschulte@trinity.edu>
2023-10-21 21:48:27 -0500

    Form Data with GET - why POST is superior

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -6,6 +6,15 @@ import play.api.mvc._
 
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+    def login = Action {
+        Ok(views.html.login1())
+    }
+
+    def validateLoginGet(username: String, password: String) = Action {
+        Ok(s"$username logged in with $password.")
+    }
+
     def taskList = Action {
         val tasks = List("task1","task2","task3","sleep","eat")
         Ok(views.html.taskList1(tasks))
