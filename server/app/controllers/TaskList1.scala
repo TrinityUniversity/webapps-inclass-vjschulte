fe866abe06aaca8953aca16b5a08f1d96bf6b370
vjschulte <vschulte@trinity.edu>
2023-10-21 20:47:29 -0500

    Video 4 Progress: Inital Webpages

---
+++ server/app/controllers/TaskList1.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+
+@Singleton
+class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+    def taskList = Action {
+        val tasks = List("task1","task2","task3","sleep","eat")
+        Ok(views.html.taskList1(tasks))
+    }
+}
