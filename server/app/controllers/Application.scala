02106046e941244bf6b4fe561d5958f5eac4a91b
vjschulte <vschulte@trinity.edu>
2023-10-22 21:31:37 -0500

    Testing through simulated Webpage

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -16,4 +16,10 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
     Ok(s"Product type is: $prodType, product number is: $prodNum")
   }
 
+  def randomNumber = Action {
+    Ok(util.Random.nextInt(100).toString)
+  }
+  def randomString(length: Int) = Action {
+    Ok(util.Random.nextString(length))
+  }
 }
