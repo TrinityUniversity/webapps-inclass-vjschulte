306a4597b1d4d9a96e4a23c6b530932cdbc82bf6
vjschulte <vschulte@trinity.edu>
2023-11-19 00:19:13 -0600

    jQuery Ajax

--- server/app/controllers/TaskList2.scala
+++ server/app/controllers/TaskList2.scala
@@ -23,4 +23,39 @@ class TaskList2 @Inject()(cc: ControllerComponents) extends AbstractController(c
             Ok(views.html.login2())
         }
     }
+
+    def createUser(username: String, password: String) = Action {
+        println("creating a user")
+        if(TaskListInMemoryModel.createUser(username, password)) {
+            Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
+        } else {
+            Ok(views.html.login2())
+        }
+    }
+
+    def deleteTask(index: Int) = Action { implicit request =>
+        val usernameOption = request.session.get("username")
+        usernameOption.map { username =>
+                TaskListInMemoryModel.removeTask(username, index);
+                Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+        }.getOrElse(Ok(views.html.login2()))
+
+    }
+
+    def addTask(task: String) = Action { implicit request =>
+        val usernameOption = request.session.get("username")
+        usernameOption.map { username =>
+                TaskListInMemoryModel.addTask(username, task);
+                Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+        }.getOrElse(Ok(views.html.login2()))
+
+    }
+
+    def logout = Action {
+        Redirect(routes.TaskList2.login).withNewSession
+    }
+
+    def generatedJS = Action {
+        Ok(views.js.generatedJS())
+    }
 }
