72eee75fb6e7e9d6a79a93eef067e3dcb1c4ee32
vjschulte <vschulte@trinity.edu>
2023-11-19 14:26:46 -0700

    Validate

--- server/app/controllers/TaskList3.scala
+++ server/app/controllers/TaskList3.scala
@@ -6,11 +6,32 @@ import play.api.mvc._
 import play.api.i18n._
 import models.TaskListInMemoryModel
 import play.api.libs.json.Json
+import models._
+import play.api.libs.json.JsSuccess
+import play.api.libs.json.JsError
 
 @Singleton
 class TaskList3 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
     def load = Action { implicit request => 
         Ok(views.html.version3Main())
+    }
+
+    implicit val userDataReads = Json.reads[UserData]
+
+    def validate = Action { implicit request =>
+        request.body.asJson.map { body => 
+            Json.fromJson[UserData](body) match {
+                case JsSuccess(ud, path) =>
+                    if(TaskListInMemoryModel.validateUser(ud.username, ud.password)) {
+                        Ok(Json.toJson(true))
+                        .withSession("username" -> ud.username, "csrfToken" -> play.filters.csrf.CSRF.getToken.get.value)
+                    } else {
+                        Ok(views.html.login2())
+                    }
+                    Ok("")
+                case e @ JsError (_) => Redirect(routes.TaskList3.load)
+            }
+        }.getOrElse(Redirect(routes.TaskList3.load))
     }
 
     def data = Action {
