14d59aa3de48fcd46bca176ed681615ea3c89599
vjschulte <vschulte@trinity.edu>
2023-11-19 01:56:11 -0700

    jQuery Ajax Final

--- server/public/javascript/version2.js
+++ server/public/javascript/version2.js
@@ -1,22 +1,50 @@
-$("#contents").load("/login2");
+const csrfToken = $("#csrfToken").val();
+const loginRoute = $("#loginRoute").val();
+const validateRoute = $("#validateRoute").val();
+const createRoute = $("#createRoute").val();
+const addRoute = $("#addRoute").val();
+const deleteRoute = $("#deleteRoute").val();
+
+
+$("#contents").load(loginRoute); 
 
 function login() {
     const username = $("#loginName").val();
     const password = $("#loginPass").val();
-    $("#contents").load("/validate2?username="+username+"&password="+password);
+    $.post(
+            validateRoute,
+            {username, password, csrfToken},
+            data => {
+                ("#contents").html(data);
+            });
 }
 
 function createUser() {
     const username = $("#createName").val();
     const password = $("#createPass").val();
-    $("#contents").load("/create2?username="+username+"&password="+password);
+    $.post(
+        createRoute,
+        {username, password, csrfToken},
+        data => {
+            ("#contents").html(data);
+        });
 }
 
 function deleteTask(index) {
-    $("#contents").load("/deleteTask2?index="+index);
+    $.post(
+        deleteRoute,
+        {index, csrfToken},
+        data => {
+            ("#contents").html(data);
+        });
 }
 
 function addTask() {
     const task = $("#newTask").val();
-    $("#contents").load("/addTask2?task="+encodeURIComponent(task));
+    $.post(
+        addRoute,
+        {text, csrfToken},
+        data => {
+            ("#contents").html(data);
+        });
 }
